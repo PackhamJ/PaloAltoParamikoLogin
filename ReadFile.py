6bc801651a830b89ee58f6385986271f3d9ef3b8
PackhamJ <viry2k@gmail.com>
2017-03-29 13:51:37 -0600

    Update ReadFile.py

--- ReadFile.py
+++ ReadFile.py
@@ -1,5 +1,5 @@
 #This method opens a file as read only and puts the values into an array and returns the array values
-#@author Jordan Packham viry2k@gmail.com
+#@author Jordan Packham viry2kphone@gmail.com
 #version .082 (This is the current version)
 #Since    .000001 (Version number that this method was added to the PaloAltoParamikoLogin class)
 #@param fileName the name of the file that this method will read through.
